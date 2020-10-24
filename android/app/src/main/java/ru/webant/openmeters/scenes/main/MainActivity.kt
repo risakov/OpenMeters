e1fb0420c973796929a98a3a1591137d6f44ade5
Stepanchenko Nikolay <nikolays@webant.ru>
2020-10-24 20:31:29 +0300

    Исправлен баг с появляющейся клавиатурой

--- android/app/src/main/java/ru/webant/openmeters/scenes/main/MainActivity.kt
+++ android/app/src/main/java/ru/webant/openmeters/scenes/main/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
 
         hideStatusBar()
         setupBottomNavigation()
