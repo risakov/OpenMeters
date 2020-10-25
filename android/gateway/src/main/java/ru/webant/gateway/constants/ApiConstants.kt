1a84e2a08e7282513753453bf58885ae57035399
Stepanchenko Nikolay <nikolays@webant.ru>
2020-10-25 05:52:33 +0300

    Изменен endpoint

--- android/gateway/src/main/java/ru/webant/gateway/constants/ApiConstants.kt
+++ android/gateway/src/main/java/ru/webant/gateway/constants/ApiConstants.kt
@@ -1,5 +1,5 @@
 package ru.webant.gateway.constants
 
 object ApiConstants {
-    const val API_URL = "http://192.168.11.212:3333"
-}
+    const val API_URL = "https://young-meters.herokuapp.com"
+}п
