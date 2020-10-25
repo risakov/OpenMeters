2c68a6d9123dc16b651c56b9f79bebff4e41f7e6
Stepanchenko Nikolay <nikolays@webant.ru>
2020-10-25 05:22:42 +0300

    Сделан splash screen

---
+++ android/app/src/main/java/ru/webant/openmeters/scenes/splash/SplashActivity.kt
@@ -0,0 +1,36 @@
+package ru.webant.openmeters.scenes.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import ru.webant.openmeters.R
+import ru.webant.openmeters.extensions.rotateInfiniteAnimation
+import ru.webant.openmeters.scenes.main.MainActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        loaderImageView.rotateInfiniteAnimation()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            run {
+                val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+                this@SplashActivity.startActivity(mainIntent)
+                this@SplashActivity.finish()
+            }
+        }, SPLASH_DISPLAY_LENGTH)
+    }
+
+
+    companion object {
+        private const val SPLASH_DISPLAY_LENGTH = 1000L
+    }
+}
